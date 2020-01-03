4f01ce38cf0e8d7b91422644d0d0f6fe829d3864
Kenny Maness <Kenny@Shep.local>
2020-01-03 16:47:38 -0600

    Revert "{to revert}"
    This reverts commit fa5ab46b7b0df5383ff6e5534cff5f73d9c20eaa.

--- src/main/java/com/kennymaness/kennymaness/daos/UserDao.java
+++ src/main/java/com/kennymaness/kennymaness/daos/UserDao.java
@@ -2,15 +2,9 @@ package com.kennymaness.kennymaness.daos;
 
 import org.springframework.data.repository.CrudRepository;
 import com.kennymaness.kennymaness.models.User;
-import java.util.ArrayList;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 public interface UserDao extends CrudRepository<User, Integer> {
-
-    User findByFirstName(String first_name);
-    User findByLastName(String last_name);
-    User findByUsername(String username);
-    User findByEmail(String email);
 
 }
